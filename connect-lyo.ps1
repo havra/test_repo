14ecc91c879a16bd9c04993430d7a32552f5c0b3
havra <havranek@gmail.com>
2014-05-07 13:05:37 -0400

    Lync CMDlet

---
+++ connect-lyo.ps1
@@ -0,0 +1,4 @@
+$Password = ConvertTo-SecureString "$PASS" -asPlainText -force
+$LiveCred = New-Object System.Management.Automation.PSCredential -ArgumentList $USERNAME,$Password
+$CSSession = New-CsOnlineSession -Credential $LiveCred
+Import-PSSession $CSSession -AllowClobber
