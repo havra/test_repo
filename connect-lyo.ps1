52bc384e6675db62f9e3f24afdfab8003faf217f
havra <havranek@gmail.com>
2014-05-07 13:07:03 -0400

    User + Pass

--- connect-lyo.ps1
+++ connect-lyo.ps1
@@ -1,3 +1,5 @@
+$USERNAME = ""
+$PASS = ""
 $Password = ConvertTo-SecureString "$PASS" -asPlainText -force
 $LiveCred = New-Object System.Management.Automation.PSCredential -ArgumentList $USERNAME,$Password
 $CSSession = New-CsOnlineSession -Credential $LiveCred
